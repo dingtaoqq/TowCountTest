39214585f9d187d6935c56e73da48b3cded6cb60
dingtao1998 <dingtao1998@sohu.com>
2018-01-06 21:16:37 +0800

    dingtaoqq提交SecondActiviy

--- app/src/main/java/dt/com/towcounttest/SecondActivity.java
+++ app/src/main/java/dt/com/towcounttest/SecondActivity.java
@@ -1,10 +1,17 @@
 package dt.com.towcounttest;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 /**
  * Created by Administrator on 2018/1/6.
  */
 
 public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
