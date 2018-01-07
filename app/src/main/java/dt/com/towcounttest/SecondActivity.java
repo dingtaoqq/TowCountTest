aa7ee4e655a777a64980364a1f90a63314a9e7cb
dingtaoqq <973169042@qq.com>
2018-01-07 13:05:50 +0800

    dingtaoqq提交SecondActiviy  测试不pull是否可以提交

--- app/src/main/java/dt/com/towcounttest/SecondActivity.java
+++ app/src/main/java/dt/com/towcounttest/SecondActivity.java
@@ -2,6 +2,7 @@ package dt.com.towcounttest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by Administrator on 2018/1/6.
@@ -11,6 +12,7 @@ public class SecondActivity extends Activity {
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TextView  textView  =  new TextView(this);
 
     }
 }
